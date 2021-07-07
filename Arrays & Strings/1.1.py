37b608ad4f5fa02ea1238e5a6009a240f1aa467d
mohit <jawale.mohit123@gmail.com>
2021-07-07 08:48:44 +0530

    Adding the code with using data structure

--- Arrays & Strings/1.1.py
+++ Arrays & Strings/1.1.py
@@ -1,8 +1,12 @@
 
-
-
-
 def find_unique_string(str):
+    """
+    Description: If the string has unique character the function will return True otherwise
+    False.
+    :param str: String input.
+    :return: Boolen value.
+    Time Complexity: O(c) where c is length of the string passed to the function.
+    """
     original_length = len(str)
     remove_duplicate = set()
     for i in range(0,original_length):
